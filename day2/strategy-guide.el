011d712954965ec3a427977d665881ddf9241471
Chris Matzenbach <matzy@protonmail.com>
2022-12-10 01:41:23 -0600

    Finished funcs to calculate scores, but not working correctly

--- day2/strategy-guide.el
+++ day2/strategy-guide.el
@@ -27,6 +27,8 @@
 
 (defvar *strategy-guide* (file-to-string "./data.el"))
 (defvar *t-strategy-guide* (file-to-string "./test-data.el"))
+(defvar *player-score* 0)
+(defvar *opponent-score* 0)
 
 (defun separate-round-data-to-lists (data)
     (remq nil (mapcar #'(lambda (round)
@@ -34,7 +36,46 @@
                          (list (substring round 0 1) (substring round 2 3))))
            (split-string data "\n"))))
 
+(defun check-choice-pts (choice)
+    (cond
+        ((or (string-equal "A" choice) (string-equal "X" choice)) 1)
+        ((or (string-equal "B" choice) (string-equal "Y" choice)) 2)
+        ((or (string-equal "C" choice) (string-equal "Z" choice)) 3)))
+
+(defun calc-round-winner (opp player)
+    (cond
+        ((or (and (string-equal "A" opp) (string-equal "X" player))
+             (and (string-equal "B" opp) (string-equal "Y" player))
+             (and (string-equal "C" opp) (string-equal "Z" player)))
+            (progn
+                (setq *player-score* (+ *player-score* 3))
+                (setq *opponent-score* (+ *opponent-score* 3))))
+        ((or (and (string-equal "A" opp) (string-equal "Y" player))
+             (and (string-equal "B" opp) (string-equal "Z" player))
+             (and (string-equal "C" opp) (string-equal "X" player)))
+            (setq *player-score* (+ *player-score* 3)))
+        ((or (and (string-equal "A" opp) (string-equal "Z" player))
+             (and (string-equal "B" opp) (string-equal "X" player))
+             (and (string-equal "C" opp) (string-equal "Y" player)))
+            (setq *opponent-score* (+ *opponent-score* 3)))))
+
+(defun calculate-scores (rounds-list)
+    (dolist (curr-round rounds-list)
+        (let ((opp (car curr-round))
+                 (player (car (cdr curr-round))))
+            (progn
+                (setq *opponent-score* (+ *opponent-score* (check-choice-pts opp)))
+                (setq *player-score* (+ *player-score* (check-choice-pts player)))
+                (calc-round-winner opp player))))
+    (progn (print *player-score*) (print *opponent-score*)))
+
 (separate-round-data-to-lists *t-strategy-guide*)
+(calculate-scores (separate-round-data-to-lists *t-strategy-guide*))
+(print *player-score*)
+(print *opponent-score*)
+
+
+
 
 ;; first-col
 ;; A - Rock
@@ -50,6 +91,7 @@
 ;; 1 - Rock
 ;; 2 - Paper
 ;; 3 - Scissors
+;; plus (+)
 ;; 0 - loss
 ;; 3 - draw
 ;; 6 - win
